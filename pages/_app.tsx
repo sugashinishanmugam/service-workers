7831605b419b1f680eb4872edc0538b25d64545a
sugashinishanmugam <sugashinishanmugam@gmail.com>
2023-05-12 15:37:25 -0500

    Service workers using nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/service-worker.js',{ scope: '/docs' })
+        .then((registration) => console.log('scope is: ', registration.scope));
+    }
+  }, []);
+
   return <Component {...pageProps} />
 }
+
